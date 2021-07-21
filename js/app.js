318808fcb4f3c6bee3ef14a17831fa7402c019c1
chitvan.jain <chitvanjain@gmail.com>
2021-07-21 23:08:57 +0530

    Some Comments

--- js/app.js
+++ js/app.js
@@ -1,20 +1,30 @@
+// The method which is called on both the Shuffle and Sort Button Clicks
 const render = (shuffle) => {
     let container = document.querySelector(".tiles");
+
+    // Convert the Node list returned to an actual array
     let tiles = Array.prototype.slice.call(document.querySelectorAll('.tile'));
+
     tiles.forEach(tile => {
         container.removeChild(tile);
     })
+
+    // Shuffle Button Clicked
     if (shuffle) {
         for (let i = tiles.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * i);
             [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
         }
     }
+
+    // Sort Button Clicked
     else {
         tiles.sort((a, b) => a.innerText - b.innerText);
     }
-    document.getElementById("btnSort").disabled = !shuffle;
+
     tiles.forEach(tile => {
         container.appendChild(tile);
     })
+
+    document.getElementById("btnSort").disabled = !shuffle;
 }
